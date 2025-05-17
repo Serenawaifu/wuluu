7fd79ce3e476a6ac266cfde5e890e4e30751e0b9
Sparkie <78498155+Serenawaifu@users.noreply.github.com>
2025-05-17 19:27:55 +0530

    Create next.config.js

---
+++ next.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'cdn.sanity.io',
+      },
+    ],
+  },
+  experimental: {
+    serverActions: true,
+  },
+};
